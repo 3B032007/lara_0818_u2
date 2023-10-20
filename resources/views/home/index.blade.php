2afdb33d4f0dd0b3667f716b5b759cab4e6d6e70
劉秉樺 <alen920617@gmail.com>
2023-10-20 10:05:51 +0800

    建立子樣板

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>lara_0818_u2</title>
+    </head>
+<body>
+    @extends('layouts.master')
+    @section('title','Home')
+    @section('content')
+    <h1>Home</h1>
+    <p>Thisismybodycontent.</p>
+    @endsection
+</body>
+</html>
--- resources/views/home/index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>lara_0818_u2</title>
-    </head>
-<body>
-    123456789
-</body>
-</html>
