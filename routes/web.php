a6764653667f173e6646a4164c9bd4506548a04e
劉秉樺 <alen920617@gmail.com>
2023-10-20 10:40:01 +0800

    建立HelloController並傳遞資料至view顯示

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +25,5 @@ Route::get('/', function () {
 Route::get('home',function(){
     return view('home.index');
 });
+
+Route::get('hello/{name?}',[HelloController::class,'index'])->name('hello.index');
